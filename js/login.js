47f6969996131ad9caf809c0fec583df146a5331
shafrinahamed97 <shafrinahamedjob@gmail.com>
2025-04-26 15:25:56 +0600

    Create login form bad setting, In future we can not this login method

--- js/login.js
+++ js/login.js
@@ -4,17 +4,41 @@
 // Login Button
 
 // Step-1: Set event handler
+// document.getElementById('btn-login').addEventListener('click', function (event) {
+//     // Step-2: prevent default behavior (prevent reloading browser)
+//     event.preventDefault(); // ভিগিনারদের জন্য
+
+//     // Step-3: get the phone number & Phone Number
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(phoneNumber, pinNumber);
+
+//     // this is temporary. You should not do like this
+//     if (phoneNumber === "5" && pinNumber === '1234') {
+//         console.log("You are logged in");
+//         // Step-5: Allow user to use the website
+
+
+//     } else {
+//         alert("Wrong phone number or pin");
+
+//     }
+
+
+
+
+
+
+// })
+
 document.getElementById('btn-login').addEventListener('click', function (event) {
-    // Step-2: prevent default behavior (prevent reloading browser)
-    event.preventDefault(); // ভিগিনারদের জন্য বেজাল এটা। 
-    console.log('login button clicked');
-
-
-    // Step-3: get the phone number
+    event.preventDefault();
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pin = document.getElementById('pin-number').value;
 
-
-
-
+    if (phoneNumber === "123456" && pin === "123546") {
+        console.log("Your Logged in");
+    } else {
+        alert("Phone Number Or PIN number Wrong")
+    }
 })
