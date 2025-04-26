cf65b0c94534195b6c31e9e04faa9faaaee6d6ac
shafrinahamed97 <shafrinahamedjob@gmail.com>
2025-04-26 21:47:44 +0600

    Verify Pin Number And Add Money to the balance

--- js/home.js
+++ js/home.js
@@ -3,7 +3,8 @@
 /*
 
 S-1: add event handler
-s-2: get money to be added to the account balance
+S-2: get money to be added to the account balance and get the pin number
+S-3: verify to the pin number
 
 
 */
@@ -17,9 +18,29 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
     // Step-2: get money to be added to the account
     const addMoneyInput = document.getElementById('input-add-money').value;
 
-    // Step-3: get the pin the number provided
+    //  get the pin the number provided
     const pinNumberInput = document.getElementById('input-pin-number').value;
     console.log(addMoneyInput, pinNumberInput);
 
+
+    // Step-3: verify pin number
+    // Wrong way to validate pin number
+    if (pinNumberInput === "1234") {
+        // Step-4: get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance);
+
+        // Step-5: add addMoneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+
+        // Step-6: update the balance in the UI/DOM
+        document.getElementById('account-balance').innerText = newBalance;
+
+        console.log(newBalance);
+    } else {
+        alert("Failed to add money! Please try again.")
+    }
 
 });
