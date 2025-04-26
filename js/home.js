6bcf5881e252cd094b81dfe9bbe008c8964641fa
shafrinahamed97 <shafrinahamedjob@gmail.com>
2025-04-26 21:23:23 +0600

    Handle add money event handler on the button click

--- js/home.js
+++ js/home.js
@@ -1,1 +1,25 @@
-console.log("Home JS Connected");
+// add money to the account
+
+/*
+
+S-1: add event handler
+s-2: get money to be added to the account balance
+
+
+*/
+
+
+// Step-1: Add an event handler to the add money button inside the form
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    // prevent page reload after form submit.
+    event.preventDefault();
+
+    // Step-2: get money to be added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+
+    // Step-3: get the pin the number provided
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(addMoneyInput, pinNumberInput);
+
+
+});
