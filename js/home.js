274f6126575ee56c6108885ff42d35f7ee038332
shafrinahamed97 <shafrinahamedjob@gmail.com>
2025-04-27 11:07:29 +0600

    Recap previous task

--- js/home.js
+++ js/home.js
@@ -11,36 +11,56 @@ S-3: verify to the pin number
 
 
 // Step-1: Add an event handler to the add money button inside the form
+// document.getElementById('btn-add-money').addEventListener('click', function (event) {
+//     // prevent page reload after form submit.
+//     event.preventDefault();
+
+//     // Step-2: get money to be added to the account
+//     const addMoneyInput = document.getElementById('input-add-money').value;
+
+//     //  get the pin the number provided
+//     const pinNumberInput = document.getElementById('input-pin-number').value;
+//     console.log(addMoneyInput, pinNumberInput);
+
+
+//     // Step-3: verify pin number
+//     // Wrong way to validate pin number
+//     if (pinNumberInput === "1234") {
+//         // Step-4: get the current balance
+//         const balance = document.getElementById('account-balance').innerText;
+//         // console.log(balance);
+
+//         // Step-5: add addMoneyInput with balance
+//         const addMoneyNumber = parseFloat(addMoneyInput);
+//         const balanceNumber = parseFloat(balance);
+//         const newBalance = addMoneyNumber + balanceNumber;
+
+//         // Step-6: update the balance in the UI/DOM
+//         document.getElementById('account-balance').innerText = newBalance;
+
+//         console.log(newBalance);
+//     } else {
+//         alert("Failed to add money! Please try again.")
+//     }
+
+// });
+
+
 document.getElementById('btn-add-money').addEventListener('click', function (event) {
-    // prevent page reload after form submit.
     event.preventDefault();
 
-    // Step-2: get money to be added to the account
-    const addMoneyInput = document.getElementById('input-add-money').value;
+    const amountAdded = document.getElementById('input-add-money').value;
+    const addedPin = document.getElementById('input-pin-number').value;
 
-    //  get the pin the number provided
-    const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(addMoneyInput, pinNumberInput);
-
-
-    // Step-3: verify pin number
-    // Wrong way to validate pin number
-    if (pinNumberInput === "1234") {
-        // Step-4: get the current balance
+    if (addedPin === "123456") {
         const balance = document.getElementById('account-balance').innerText;
-        console.log(balance);
-
-        // Step-5: add addMoneyInput with balance
-        const addMoneyNumber = parseFloat(addMoneyInput);
-        const balanceNumber = parseFloat(balance);
-        const newBalance = addMoneyNumber + balanceNumber;
-
-        // Step-6: update the balance in the UI/DOM
-        document.getElementById('account-balance').innerText = newBalance;
-
-        console.log(newBalance);
+        const newAmountAdded = parseFloat(amountAdded);
+        const currentBalance = parseFloat(balance);
+        const updatedBalance = newAmountAdded + currentBalance;
+        document.getElementById('account-balance').innerText = updatedBalance;
     } else {
-        alert("Failed to add money! Please try again.")
+        alert("Something went wrong. Please! Try again ")
     }
 
-});
+
+})
