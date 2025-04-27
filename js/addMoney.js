33659a17fbd5e4e32afd31e18160d2fbc4ea4060
shafrinahamed97 <shafrinahamedjob@gmail.com>
2025-04-27 11:24:31 +0600

    Implement Cash Out Feature and Successfully end this task

--- js/addMoney.js
+++ js/addMoney.js
@@ -19,6 +19,7 @@ also, get the pin number provided
 
 
 //  Add Money
+
 document.getElementById('btn-add-money').addEventListener("click", function (event) {
     event.preventDefault();
 
@@ -48,6 +49,24 @@ document.getElementById('btn-add-money').addEventListener("click", function (eve
 
 
 // Cash Out
+
+document.getElementById('btn-cash-out').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const cashOutBalance = document.getElementById('input-cash-out-money').value;
+    const pinNumber = document.getElementById('cash-out-pin-number').value;
+
+    if (pinNumber === "123") {
+        const balance = document.getElementById('account-balance').innerText;
+        const previousBalance = parseFloat(balance);
+        const withdrawBalance = parseFloat(cashOutBalance);
+
+        const newBalance = previousBalance - withdrawBalance;
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+
+});
+
 
 
 
